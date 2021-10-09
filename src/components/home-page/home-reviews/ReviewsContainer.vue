3fd145a86f9ce9c5ada2ae6de60fdf7036bc4c9c
Death <M.26A20.M@Gmail.com>
2021-10-10 00:24:42 +0330

    Tweaked components

--- src/components/home-page/home-reviews/ReviewsContainer.vue
+++ src/components/home-page/home-reviews/ReviewsContainer.vue
@@ -24,11 +24,7 @@
 					<a href="#" class="text-black hover:underline">{{ hoveredReview[0].name }}</a>
 				</p>
 				<p class="max-w-xs mb-8 text-xs text-gray-700 fon font-IranSans lg:text-sm">{{ hoveredReview[0].review }}</p>
-				<a
-					href="#"
-					class="px-8 py-3 text-white transition-all duration-200 bg-blue-400 border rounded-full shadow-sm hover:bg-blue-500 hover:shadow-lg font-IranSans"
-					>نمایش همه
-				</a>
+				<a href="#" class="px-8 py-3 text-white transition-all duration-200 border rounded-full review-button font-IranSans">نمایش همه </a>
 			</div>
 		</div>
 	</div>
@@ -77,3 +73,24 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+.review-button {
+	--border-opacity: 1;
+	--bg-opacity: 1;
+	--text-opacity: 1;
+	background-color: rgba(50, 138, 241, var(--bg-opacity));
+	border-color: rgba(50, 138, 241, var(--border-opacity));
+	color: rgba(255, 255, 255, var(--text-opacity));
+}
+
+.review-button:hover {
+	--border-opacity: 1;
+	--bg-opacity: 1;
+	--text-opacity: 1;
+	background-color: rgba(39, 121, 189, var(--bg-opacity));
+	border-color: rgba(50, 138, 241, var(--border-opacity));
+	box-shadow: 0 0 10px rgb(0 0 0 / 25%);
+	color: rgba(255, 255, 255, var(--text-opacity));
+}
+</style>
