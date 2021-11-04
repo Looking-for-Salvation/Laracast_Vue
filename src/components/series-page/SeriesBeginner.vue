610b493e69a0d22ae024b2dee0ec511f1f5ab8ce
Death <M.26A20.M@Gmail.com>
2021-11-05 00:00:04 +0330

    Tweaked SeriesBeginner

--- src/components/series-page/SeriesBeginner.vue
+++ src/components/series-page/SeriesBeginner.vue
@@ -1,6 +1,6 @@
 <template>
 	<section>
-		<header class="container text-center mb-4 mx-auto max-w-[650px] pb-4 text-black font-IranSans">
+		<header class="container text-center mb-4 mx-auto max-w-[650px] pb-4 text-black font-IranSans hover:text-white">
 			<h3 class="mb-2 text-3xl inherits-color md:font-light">تازه واردی؟</h3>
 			<p class="font-light inherits-color text-2lg">تازه وارد جمع لاراکست شدی؟ این دوتا دوره واسه این که شروع کنی.</p>
 		</header>
