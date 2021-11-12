9af49f7104c407cb936a81dc677a738a0f85280f
Death <M.26A20.M@Gmail.com>
2021-11-12 13:38:45 +0330

    Added Vuex home module

--- src/store/modules/modals/getters.js
+++ src/store/modules/modals/getters.js
@@ -1,23 +1,34 @@
 export default {
+	//* LoginModal related getters
 	loginModalVisibility(state) {
 		return state.isLoginVisible;
 	},
+
+	//* JoinModal related getters
 	joinModalVisibility(state) {
 		return state.isJoinVisible;
 	},
+	joinModalCurrentTab(state) {
+		return state.joinModalCurrentTab;
+	},
+
+	//* SignupModal related getters
 	signupModalVisibility(state) {
 		return state.isSignupVisible;
 	},
+
+	//* ForgotPasswordModal related getters
 	forgotPasswordModalVisibility(state) {
 		return state.isForgotPasswordVisible;
 	},
+
+	//* MobileNavModal related getters
 	mobileNavModalVisibility(state) {
 		return state.isMobileNavVisible;
 	},
+
+	//* SupportModal related getters
 	supportModalVisibility(state) {
 		return state.isSupportVisible;
-	},
-	joinModalCurrentTab(state) {
-		return state.joinModalCurrentTab;
 	},
 };
